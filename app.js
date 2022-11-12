0e2bc9b6242f1a3070a5c25615f6e2c3da6d445d
MemoLee <softwarewewo@gmail.com>
2022-11-13 01:08:08 +0300

    run

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// MIDDLEWARES
+//MIDDLEWARES
+/* request - response döngüsünün içerisindeki görevi olan her fonksiyona 
+middleware denir. Yani herşey request ve responsun 'middle'ında ortasında yapılır. */
 app.use(express.static('public'));
 
-app.get('/',(req,res) =>{
-    res.sendFile(path.resolve(__dirname,'temp/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
-const port = 5001;
-app.listen(port,() =>{
+const port = 3000;
+app.listen(port, () => {
     console.log(`Blog app sunucu ${port} portunda başlatıldı...`);
 });
