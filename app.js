45f722cf17864ddf30347505d77730134ab3fe06
MemoLee <softwarewewo@gmail.com>
2022-11-14 10:16:03 +0300

    temp engine added

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+
+// TEMPLATES
+app.set('view engine','ejs');
 //MIDDLEWARES
 /* request - response döngüsünün içerisindeki görevi olan her fonksiyona 
 middleware denir. Yani herşey request ve responsun 'middle'ında ortasında yapılır. */
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index')
+});
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+app.get('/addpost', (req, res) => {
+    res.render('add_post')
+});
+app.get('/post', (req, res) => {
+    res.render('post')
 });
 
 const port = 3000;
